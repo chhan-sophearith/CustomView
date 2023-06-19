93a7fbe876d8ec13b7911db3df6666fdeee333c2
Chhan Sophearith <44798886+chhan-sophearith@users.noreply.github.com>
2023-06-19 17:04:35 +0700

    Initial Commit

---
+++ CustomView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomView
+//
+//  Created by Chhan Sophearith on 19/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CustomViewUITests/CustomViewUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CustomViewUITestsLaunchTests.swift
+//  CustomViewUITests
+//
+//  Created by Chhan Sophearith on 19/6/23.
+//
+
+import XCTest
+
+final class CustomViewUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
