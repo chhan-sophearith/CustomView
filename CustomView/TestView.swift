4feb54aa0990c0efe52ab6c858a38fbbc27816c2
Chhan Sophearith <44798886+chhan-sophearith@users.noreply.github.com>
2023-06-22 14:17:55 +0700

    init

---
+++ CustomView/TestView.swift
@@ -0,0 +1,14 @@
+//
+//  TestView.swift
+//  CustomView
+//
+//  Created by Chhan Sophearith on 19/6/23.
+//
+
+import UIKit
+
+class TestView: UIView {
+
+    @IBOutlet weak var testingButton: UIButton!
+    
+}
--- CustomView/ViewController.swift
+++ CustomView/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let testView: TestView = {
+        var view = TestView()
+        view = .fromNib()
+        view.backgroundColor = .systemPink
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        testView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(testView)
+        testView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        testView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        testView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        testView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
     }
 
 
 }
 
+extension UIView {
+    class func fromNib<T: UIView>() -> T {
+        return Bundle(for: T.self).loadNibNamed(String(describing: T.self), owner: nil, options: nil)![0] as! T
+    }
+}
